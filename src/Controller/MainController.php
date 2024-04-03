161e67ec866d2338f481c4013cff16b9e5493abf
AnaelleChiles <118526850+AnaelleChiles@users.noreply.github.com>
2024-04-03 14:59:47 +0200

    Fin C05

---
+++ src/Controller/MainController.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+
+
+class MainController extends AbstractController
+{
+    #[Route('/')]
+    public function HomePage() : Response {
+        $starshipCount = 457;
+        $myShip = [
+            "top" => "Sun",
+            "bottom" => "Moon",
+            'thirdWheel' => "Y/N",
+        ];
+        return $this->render("main/homepage.html.twig", ["numberOfStarships" => $starshipCount, 'myShip' => $myShip]);
+    }
+}
